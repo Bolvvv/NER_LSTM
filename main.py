c6be6bcf3179cdcea17a28f5e58bc4ad4d4b92f0
fy <a>
2020-11-01 15:15:08 +0800

    初始化代码

---
+++ main.py
@@ -0,0 +1,29 @@
+from processing_data import build_corpus
+from utils import extend_maps, prepocess_data_for_lstmcrf
+from evaluate import bilstm_train_and_eval, ensemble_evaluate
+def main():
+    # 读取数据
+    print("读取数据...")
+    train_word_lists, train_tag_lists, word2id, tag2id = build_corpus("train")
+    dev_word_lists, dev_tag_lists = build_corpus("dev", make_vocab=False)
+    test_word_lists, test_tag_lists = build_corpus("test", make_vocab=False)
+
+    # 训练评估BI-LSTM模型
+    print("正在训练评估双向LSTM模型...")
+    # LSTM模型训练的时候需要在word2id和tag2id加入PAD和UNK
+    bilstm_word2id, bilstm_tag2id = extend_maps(word2id, tag2id, for_crf=False)
+    lstm_pred = bilstm_train_and_eval(
+        (train_word_lists, train_tag_lists),
+        (dev_word_lists, dev_tag_lists),
+        (test_word_lists, test_tag_lists),
+        bilstm_word2id, bilstm_tag2id,
+        crf=False
+    )
+
+    ensemble_evaluate(
+        [lstm_pred],
+        test_tag_lists
+    )
+
+if __name__ == "__main__":
+    main()
